4f632b22f331d20f863f10aa54169686aac7b846
billkabanga <tkbillkabanga@gmail.com>
2019-09-05 13:02:59 +0300

    Initial commit

---
+++ README.md
@@ -0,0 +1,1 @@
+#### basic app with react native
---
+++ src/components/AuthForm.js
@@ -0,0 +1,9 @@
+import React, { Component } from 'react';
+import { StyleSheet, Text, View, TextInput, TouchableOpacity, AsyncStorage, Keyboard } from 'react-native';
+
+export default class AuthForm extends Component {
+  state = {
+    email: '',
+    password: '',
+  };
+}
